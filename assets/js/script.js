234a508046345220fdd96b00a8ac0eaa44ddf866
chris-townsend <chris-townsend@live.co.uk>
2022-07-06 11:53:53 +0000

    Fix modal showing on homepage when instructions box is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,25 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overLay = document.getElementById('overlay')
+// Get the modal
+var modal = document.getElementById("myModal");
 
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
-})
+// Get the button that opens the modal
+var btn = document.getElementById("instruction-btn");
 
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.modal')
-        closeModal(modal)
-    })
-})
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-function openModal(modal) {
-if (modal == null) return
-modal.classList.add('active')
-overLay.classList.add('active')
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
-function closeModal(modal) {
-    if (modal == null) return
-    modal.classList.remove('active')
-    overLay.classList.remove('active')
-    }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
