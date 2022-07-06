652ccf7d12cf7483f320fd3c14044166aeb76b82
chris-townsend <chris-townsend@live.co.uk>
2022-07-06 10:36:31 +0000

    Add modal to instructions box on homepage

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overLay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+if (modal == null) return
+modal.classList.add('active')
+overLay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overLay.classList.remove('active')
+    }
