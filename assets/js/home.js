805d5a9f5736f4f9db215a254acc68cc84fce974
chris-townsend <chris-townsend@live.co.uk>
2022-08-02 16:46:00 +0000

    Change highscores button and icon to contact and change js and css code ids and classes from highscore to contact

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,5 @@
+//A
+
 function sendMail() {
   const params = {
    from_name : document.getElementById('fullName').value,
@@ -10,54 +12,51 @@ function sendMail() {
 })
  }
 
+//Instructions & Contact modals 
 
- 
-
-//High Scores & Instructions modals 
-
-// Get the Instructions modal
+// Get the instructions modal
 const modal = document.getElementById("myModal");
 
-// Get the Highscores modal
-const highscoreModal = document.getElementById("myhighScoreModal");
+// Get the contact modal
+const contactModal = document.getElementById("mycontactModal");
 
-// Get the button that opens the Highscores modal
-const highscoreBtn = document.getElementById("highscore");
+// Get the button that opens the contact modal
+const contactBtn = document.getElementById("contact");
 
-// Get the button that opens the Instructions modal
+// Get the button that opens the instructions modal
 const instructionBtn = document.getElementById("instruction-btn");
 
-// Get the <span> element that closes the Instructions modal
+// Get the <span> element that closes the instructions modal
 const span = document.getElementsByClassName("close")[0];
 
-// Get the <span> element that closes the Highscore modal
-const highscoreSpan = document.getElementsByClassName("highscoreClose")[0];
+// Get the <span> element that closes the contact modal
+const contactSpan = document.getElementsByClassName("contactClose")[0];
 
-// When the user clicks on the button, open the Instructions modal
+// When the user clicks on the button, open the instructions modal
 instructionBtn.onclick = function() {
   modal.style.display = "block";
 };
-// When the user clicks on the button, open the Highscores modal
-highscoreBtn.onclick = function() {
-  highscoreModal.style.display = "block";
+// When the user clicks on the button, open the contact modal
+contactBtn.onclick = function() {
+  contactModal.style.display = "block";
 };
-// When the user clicks on <span> (x), close the Instructions modal
+// When the user clicks on <span> (x), close the instructions modal
 span.onclick = function() {
   modal.style.display = "none";
 };
-// When the user clicks on <span> (x), close the Highscores modal
-highscoreSpan.onclick = function() {
-  highscoreModal.style.display = "none";
+// When the user clicks on <span> (x), close the contact modal
+contactSpan.onclick = function() {
+  contactModal.style.display = "none";
 };
-// When the user clicks anywhere outside of the Instructions modal, close it
+// When the user clicks anywhere outside of the instructions modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none"; } 
 };
-// When the user clicks anywhere outside of the Highscores modal, close it
+// When the user clicks anywhere outside of the contact modal, close it
 window.addEventListener("click", function(event) {
-  if (event.target == highscoreModal) {
-    highscoreModal.style.display = "none"; }
+  if (event.target == contactModal) {
+    contactModal.style.display = "none"; }
   });
 
  
