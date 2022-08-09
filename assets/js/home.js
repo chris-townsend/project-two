3dfc2d3d9e4718849615f83fb09cc05bd2b71167
chris-townsend <chris-townsend@live.co.uk>
2022-08-09 10:40:22 +0000

    Take out unused code on sendMail function on home.js file

--- assets/js/home.js
+++ assets/js/home.js
@@ -8,9 +8,8 @@ function sendMail() {
   /* 'service_c3qpq6z' personal ID for email service
    * 'template_jk2d9nh' personal ID for email template
    */
-  emailjs.send('service_c3qpq6z', 'template_jk2d9nh', params).then(function (res) {
-    alert("Success! Your email has been sent ");
-
+  emailjs.send('service_c3qpq6z', 'template_jk2d9nh', params)(function () {
+    alert("Success! Your email has been sent");
   });
 }
 
